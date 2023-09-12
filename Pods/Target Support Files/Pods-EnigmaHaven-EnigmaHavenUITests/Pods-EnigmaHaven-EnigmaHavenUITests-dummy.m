8d9bbf4dcc3f09325e75339bb4e0a6d2405d399a
yagrariksa <luckyrama0@gmail.com>
2023-09-12 14:23:18 +0700

    add .gitignore

--- Pods/Target Support Files/Pods-EnigmaHaven-EnigmaHavenUITests/Pods-EnigmaHaven-EnigmaHavenUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EnigmaHaven_EnigmaHavenUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EnigmaHaven_EnigmaHavenUITests
-@end
--- Pods/Target Support Files/Pods-EnigmaHaven/Pods-EnigmaHaven-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EnigmaHaven : NSObject
-@end
-@implementation PodsDummy_Pods_EnigmaHaven
-@end
